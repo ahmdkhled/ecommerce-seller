55d53a6c898fa3ab41f2c9c2649b703997815541
ahmdkhled <ahmdkhled@gmail.com>
2019-03-19 20:41:24 +0200

    add bottomNavigationView to main activity

--- app/src/main/java/com/example/ecommerceseller/MainActivity.java
+++ app/src/main/java/com/example/ecommerceseller/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.ecommerceseller;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView=findViewById(R.id.mainBottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return false;
+            }
+        });
     }
 }
