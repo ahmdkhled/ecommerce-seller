c2bf2e2582bf084d41d52bccda3ba83c8b05b6c5
ahmdkhled <ahmdkhled@gmail.com>
2019-03-21 19:58:36 +0200

    add collapsing toolbar to main activity

--- app/src/main/java/com/example/ecommerceseller/ui/MainActivity.java
+++ app/src/main/java/com/example/ecommerceseller/ui/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 
@@ -21,22 +22,26 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottomNavigationView=findViewById(R.id.mainBottomNavigation);
+        toolbar=findViewById(R.id.mainToolbar);
+        setSupportActionBar(toolbar);
+
         OrdersFrag ordersFrag= (OrdersFrag) getSupportFragmentManager().findFragmentByTag("orders_tag");
         if (ordersFrag==null){
             getSupportFragmentManager()
                     .beginTransaction()
-                    .add(R.id.activity_main,new OrdersFrag(),"orders_tag")
+                    .add(R.id.container,new OrdersFrag(),"orders_tag")
                     .commit();
         }
 
 
 
-        bottomNavigationView=findViewById(R.id.mainBottomNavigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
